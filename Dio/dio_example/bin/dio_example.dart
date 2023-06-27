701c35d602f1d0fa850d6e81cbc805dd138f2f04
Madhan-Rkv <madhanrkv10@gmail.com>
2023-06-27 09:21:48 +0530

    dio examples

---
+++ Dio/dio_example/bin/dio_example.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+/// more samples https://github.com/cfug/dio/blob/main/example/lib/cancel_request.dart
+import 'package:dio/dio.dart';
+
+void main() async {
+  final dio = Dio();
+  dio.interceptors.add(LogInterceptor());
+  // Token can be shared with different requests.
+  final token = CancelToken();
+  // In one minute, we cancel!
+  Timer(Duration(milliseconds: 500), () {
+    token.cancel('cancelled');
+  });
+
+  // The follow three requests with the same token.
+  final url1 = 'https://pub.dev';
+  final url2 = 'https://dart.dev';
+  final url3 = 'https://flutter.dev';
+
+  await Future.wait([
+    dio
+        .get(url1, cancelToken: token)
+        .then((response) => print('${response.requestOptions.path}: succeed!'))
+        .catchError(
+      (e) {
+        if (CancelToken.isCancel(e)) {
+          print('$url1: $e');
+        }
+      },
+    ),
+    dio
+        .get(url2, cancelToken: token)
+        .then((response) => print('${response.requestOptions.path}: succeed!'))
+        .catchError((e) {
+      if (CancelToken.isCancel(e)) {
+        print('$url2: $e');
+      }
+    }),
+    dio
+        .get(url3, cancelToken: token)
+        .then((response) => print('${response.requestOptions.path}: succeed!'))
+        .catchError((e) {
+      if (CancelToken.isCancel(e)) {
+        print('$url3: $e');
+      }
+      print(e);
+    })
+  ]);
+}
---
+++ Dio/dio_example/test/dio_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:dio_example/dio_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
